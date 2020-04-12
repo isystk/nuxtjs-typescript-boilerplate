87e626f5593636392c912bd8b400efb1c859a93d
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-04-12 22:14:23 +0900

    バージョンを再進化

--- src/components/parts/LineChart.vue
+++ src/components/parts/LineChart.vue
@@ -6,8 +6,8 @@ import { Line, Bar, mixins } from "vue-chartjs";
 
 @Component({})
 export default class LineChartComponent extends Mixins(Line, Bar, mixins.reactiveProp) {
-  @Prop() public chartData: Chart.ChartData = {};
-  @Prop() public chartOptions: Chart.ChartOptions = {};
+  @Prop() public chartData!: Chart.ChartData;
+  @Prop() public chartOptions!: Chart.ChartOptions;
 
   public mounted() {
     console.log(this);
