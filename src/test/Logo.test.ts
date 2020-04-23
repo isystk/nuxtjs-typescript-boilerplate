e20813b41e3f915c1e54f0bbad0adc092deb1cad
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-04-23 17:40:42 +0900

    Logoのテストケース追加

--- src/test/Logo.test.ts
+++ src/test/Logo.test.ts
@@ -1,9 +1,26 @@
-import { mount, shallowMount } from "@vue/test-utils";
+import { mount, shallowMount, RouterLinkStub } from "@vue/test-utils";
+import Vue from "vue";
+import VueI18n from "vue-i18n";
 import Logo from "@/components/Logo.vue";
+import * as C from "@/common/constants/";
+
+Vue.prototype.$C = C;
+Vue.use(VueI18n);
+
+// VueI18nオブジェクトを作成する
+const i18n = new VueI18n({
+  locale: "ja",
+  messages: {
+    ja: require("@/locales/ja.json")
+  }
+});
 
 describe("Logo", (): void => {
   test("Vueインスタンスが生成されること", (): void => {
-    const wrapper = mount(Logo);
+    const wrapper = shallowMount(Logo, {
+      i18n,
+      stubs: { NuxtLink: RouterLinkStub }
+    });
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
